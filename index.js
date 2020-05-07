f3bcb2a8a5152ea3c3c87e1cb152fc1429e6bd2e
Christian Gomez Martinez <gomez9289@gmail.com>
2020-05-06 20:30:07 -0400

    render questions

--- index.js
+++ index.js
@@ -1,41 +1,77 @@
+//start button and timmer
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
-var startBtn = document.querySelector("start");
+var startBtn = document.querySelector("#start");
 var secondsLeft = 10;
 
 function setTime() {
-  var timerInterval = setInterval(function() {
+  var setTimeout = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft + " seconds left.";
 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
+    if (secondsLeft === 0) {
+      clearTimeout(setTimeout);
     }
   }, 1000);
 }
 
-document.getElementById("start").onclick = function setTime() {
-    timeEl.textContent = " ";
-    mainEl.appendChild("time is up");
-  }
+startBtn.onclick = setTime;
 
-start.addEventListener("click", myFunction);
-function myFunction(setTime)
+//answer buttons
+var buttonA = document.querySelector("#buttonA");
+var buttonB = document.querySelector("#buttonB");
+var buttonC = document.querySelector("#buttonC");
+var buttonD = document.querySelector("#buttonD");
+var question = document.querySelector("#question");
 
-  //questions
+var questionDiv = document.getElementById("question");
+var choicesDiv = document.getElementById("choices");
 
-  var quizQuestions = [
-      {"In which city is friends set?": 
-        "LA", "NY", "Miami", "Seattle"}
+//questions
+var questions = [
+  {
+    question: "In which city is friends set?",
+    choices: ["LA", "NY", "Miami", "Seattle"],
+    answer: "NY"
+  },
 
-      {"What is the name of the coffe shop used by the characters?": 
-        "St. James Perk", "Central Perk", "Froth of Khan", "Daily Grind"}
+  {
+    q: "What is the name of the coffe shop used by the characters?",
+    choices: ["St. James Perk", "Central Perk", "Froth of Khan", "Daily Grind"],
+    answer: "Central Perk"
+  },
 
-      {"What pet did Ross own?": 
-        "Monkey", "Lizard", "Rabit", "dog"}
+  {
+    q: "What pet did Ross own?",
+    choices: ["Monkey", "Lizard", "Rabit", "dog"],
+    answer: "Monkey"
+  },
 
-      {"What does Joey never share?": 
-        "His books", "His information", "His food", "His dvd's"}
-  ]
+  {
+    q: "What does Joey never share?",
+    choices: ["His books", "His information", "His food", "His dvd's"],
+    answer: "His food"
+  },
+];
+
+
+var index = 0
+
+function renderQuestion() {
+    var currentQuestion = questions[0];
+
+    questionDiv.textContent = currentQuestion.question.q;
+
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
+        var choice = currentQuestion.choices[i];
+
+        var btn = document.createElement("button");
+
+        btn.textContent = "LA";
+        choicesDiv.appendChild(buttonA);
+
+        renderQuestion();
+    }    
+}
+
 
